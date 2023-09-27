54559b2fff55f25113834e2db064ae6a5ea5ce17
WinVanta <piriya.c@vanta-capital.com>
2023-09-27 15:24:21 +0700

    clean aat library

--- hw/common/include/aat_defines.hpp
+++ hw/common/include/aat_defines.hpp
@@ -32,66 +32,9 @@
 #endif
 
  #define NUM_SYMBOL (512)
-//#define NUM_SYMBOL (1024)
-//#define NUM_preRisk (1024) // for orderEntry
-//#define NUM_DIM_HT (4) // for dimension hashtable memHandler
-#define NUM_preRisk (1024) // for orderEntry
-#define NUM_DIM_HT (2) // for dimension hashtable memHandler
-#define banlistNb (512)
 
 
-#define testOuchActive 0 //to compile HW must set 0 for tb set 1
 
-#define PRICE_EXPONENT (0.00001)
-#define START_TRADING (34200000000) // 09:30:00 in microseconds since midnight (9.5*60*60*1000000)
-#define END_TRADING (57600000000)   // 16:00:00 in microseconds since midnight (16*60*60*1000000)
-
-#define NUM_LEVEL (10)
-#define LEVEL_UNSPECIFIED (-1)
-#define NUM_TEST_SAMPLE (300)
-
-enum ORDERBOOK_ACTIONCODES
-{
-    ORDERBOOK_NEW = 'N',
-    ORDERBOOK_CHANGE = 'C',
-    ORDERBOOK_DELETE = 'D',
-};
-
-enum ORDER_SIDES
-{
-    ORDER_BID = 'B',
-    ORDER_ASK = 'A'
-};
-
-enum PRICINGENGINE_STRATEGIES
-{
-    STRATEGY_NONE = 0,
-    STRATEGY_PEG = 1,
-    STRATEGY_LIMIT = 2
-};
-
-enum ORDERENTRY_OPCODES
-{
-    ORDERENTRY_ADD = 0,
-    ORDERENTRY_MODIFY,
-    ORDERENTRY_DELETE
-};
-
-enum TCP_TXSTATUS_CODES
-{
-    TXSTATUS_SUCCESS = 0,
-    TXSTATUS_CLOSED,
-    TXSTATUS_OUTOFSPACE
-};
-
-enum TriggerEnum
-{
-	None = 0,
-	Up100 = 1,
-	AskMiss = 2,
-	Down100 = 3,
-	BidMiss = 4
-};
 
 
 
